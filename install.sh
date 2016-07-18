a16196d65a615e5278aeb0152034cba2dcabf96e
Hugo Carreira <hugo.colombo23@gmail.com>
2016-07-18 11:39:07 -0300

    removed google chrome added chromium browser / refactoring

--- install.sh
+++ install.sh
@@ -2,37 +2,34 @@
 
 # Instalação Padrão
 echo -e "\e[00;31mCurl é necessario, será instalado.\e[00m"
-sudo apt-get install -y php5-curl
+sudo apt-get install curl
 
 echo -e "\e[00;31mGit é necessario, será instalado.\e[00m"
-sudo apt-get install -y git
+sudo apt-get install git
 
 # Chrome Browser
-echo -e "\e[00;31mDeseja instalar Chrome? [Y/n]\e[00m"
+echo -e "\e[00;31mDeseja instalar Chromium Browser? [y/n]\e[00m"
 read answer
-if [[ $answer =~ "Y" ]]; then
-    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-    sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-    sudo apt-get update
-    sudo apt-get install -y google-chrome-stable
+if [[ $answer =~ "y" ]]; then
+    sudo apt-get install chromium-browser
 fi
 
 # Spotify
-echo -e "\e[00;31mDeseja instalar Spotify? [Y/n]\e[00m"
+echo -e "\e[00;31mDeseja instalar Spotify? [y/n]\e[00m"
 read answer
-if [[ $answer =~ "Y" ]]; then
+if [[ $answer =~ "y" ]]; then
 	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
 	echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 	sudo apt-get update
-	sudo apt-get install -y spotify-client
+	sudo apt-get install spotify-client
 fi
 # Sublime Text 3
-echo -e "\e[00;31mDeseja instalar Sublime Text 3? [Y/n]\e[00m"
+echo -e "\e[00;31mDeseja instalar Sublime Text 3? [y/n]\e[00m"
 read answer
-if [[ $answer =~ "Y" ]]; then
+if [[ $answer =~ "y" ]]; then
 	sudo add-apt-repository ppa:webupd8team/sublime-text-3
   	sudo apt-get update
-  	sudo apt-get install -y sublime-text-installer
+  	sudo apt-get install sublime-text-installer
   	echo -e "\e[01;33mPackages para serem instalados Sublime (./packages-sublime.txt)\e[1;33m"
   	echo -e "Color Scheme
 	  	Additional PHP Snippets
@@ -61,20 +58,20 @@ fi
 
 
 # Terminator
-echo -e "\e[00;31mDeseja instalar Terminator? [Y/n]\e[00m"
+echo -e "\e[00;31mDeseja instalar Terminator? [y/n]\e[00m"
 read answer
-if [[ $answer =~ "Y" ]]; then
-	sudo add-apt-repository -y ppa:gnome-terminator
+if [[ $answer =~ "y" ]]; then
+	sudo add-apt-repository ppa:gnome-terminator
 	sudo apt-get update
-	sudo apt-get install -y terminator
+	sudo apt-get install terminator
 fi
 
 # Zsh with theme
-echo -e "\e[00;31mDeseja instalar oh-my-zsh com Honukai Theme? [Y/n]\e[00m"
+echo -e "\e[00;31mDeseja instalar oh-my-zsh com Honukai Theme? [y/n]\e[00m"
 read answer
-if [[ $answer =~ "Y" ]]; then
+if [[ $answer =~ "y" ]]; then
 	sudo apt-get update
-	sudo apt-get install -y zsh
+	sudo apt-get install zsh
 	sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 	cd ~/Downloads
 	git clone https://github.com/hugocarreira/oh-my-zsh-theme.git zsh-theme
@@ -85,12 +82,12 @@ if [[ $answer =~ "Y" ]]; then
 fi
 
 # Vagrant without Box
-echo -e "\e[00;31mDeseja instalar Vagrant? [Y/n]\e[00m"
+echo -e "\e[00;31mDeseja instalar Vagrant? [y/n]\e[00m"
 read answer
-if [[ $answer =~ "Y" ]]; then
+if [[ $answer =~ "y" ]]; then
 	sudo apt-get update
-	sudo apt-get install -y virtualbox
-	sudo apt-get install -y virtualbox-dkms
+	sudo apt-get install virtualbox
+	sudo apt-get install virtualbox-dkms
 	cd ~/Downloads
 	wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
 	sudo dpkg -i vagrant_1.8.1_x86_64.deb
@@ -98,21 +95,21 @@ if [[ $answer =~ "Y" ]]; then
 fi
 
 # Htop
-echo -e "\e[00;31mDeseja instalar Htop [Y/n]\e[00m"
+echo -e "\e[00;31mDeseja instalar Htop [y/n]\e[00m"
 read answer
-if [[ $answer =~ "Y" ]]; then
+if [[ $answer =~ "y" ]]; then
 	sudo apt-get update
-	sudo apt-get install -y htop
+	sudo apt-get install htop
 fi
 
 # NodeJS
-echo -e "\e[00;31mDeseja instalar NodeJS 6x [Y/n]\e[00m"
+echo -e "\e[00;31mDeseja instalar NodeJS 6x [y/n]\e[00m"
 read answer
-if [[ $answer =~ "Y" ]]; then
+if [[ $answer =~ "y" ]]; then
 	curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-	sudo apt-get install -y nodejs
+	sudo apt-get install nodejs
 	sudo apt-get update
-	sudo apt-get install -y build-essential
+	sudo apt-get install build-essential
 fi
 
 # Auto remove padrão.
